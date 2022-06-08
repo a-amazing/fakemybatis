30d1ac47e939f00b19447b5f5301b9ce5181b7ed
wang_yi <35034350+a-amazing@users.noreply.github.com>
2022-06-08 01:31:31 +0000

    add simple ProxyHandler and ProxyFactory

---
+++ fakemybatis/src/test/java/io/tech/fakemybatis/test/ApiTest.java
@@ -0,0 +1,29 @@
+package io.tech.fakemybatis.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.tech.fakemybatis.binding.MapperProxyFactory;
+import io.tech.fakemybatis.test.dao.IUserDao;
+
+public class ApiTest {
+    private static final Logger logger = LoggerFactory.getLogger(ApiTest.class);
+
+    @Test
+    public void testUserDao() {
+        MapperProxyFactory<IUserDao> factory = new MapperProxyFactory<>(IUserDao.class);
+        Map<String, String> sqlSession = new HashMap<>();
+        sqlSession.put("io.tech.fakemybatis.test.dao.IUserDao.queryUserName", "模拟执行 Mapper.xml 中 SQL 语句的操作：查询用户姓名");
+        sqlSession.put("io.tech.fakemybatis.test.dao.IUserDao.queryUserAge", "模拟执行 Mapper.xml 中 SQL 语句的操作：查询用户年龄");
+        IUserDao userDao = factory.newInstance(sqlSession);
+
+        String res = userDao.queryUserName("10001");
+        //logger.info("测试结果：{}", res);
+        System.out.println("测试结果：" + res);
+    }
+
+}
---
+++ fakemybatis/src/test/java/io/tech/fakemybatis/test/dao/IUserDao.java
@@ -0,0 +1,9 @@
+package io.tech.fakemybatis.test.dao;
+
+public interface IUserDao {
+
+    String queryUserName(String uId);
+
+    Integer queryUserAge(String uId);
+
+}
